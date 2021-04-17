7de44cdc4b1e00b69b020b99d80dffaaca739e02
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2021-04-17 11:18:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,22 @@ function take_snapshot(){
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-54eIp3JX/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/UTWSZhVyx/model.json", modelLoaded);
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+function modelLoaded(){
+    console.log("model loaded!");
+}
